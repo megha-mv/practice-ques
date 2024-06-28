138d5449d6ea4afc7730c9bef6c601cb08696f6f
mvashishth <mvashishth@chegg.com>
2024-06-28 09:11:33 +0530

    some changes

--- uglyNum.js
+++ uglyNum.js
@@ -16,3 +16,44 @@ console.log(isUgly(6));    // true (6 = 2 * 3)
 console.log(isUgly(8));    // true (8 = 2 * 2 * 2)
 console.log(isUgly(14));   // false (14 has prime factors 2 and 7)
 console.log(isUgly(1)); 
+
+
+const uglyNumAnother = (num) => {
+  if (num <= 0) return false;
+
+  const primeFactors = [2, 3, 5];
+
+  while (primeFactors.some((factor) => num % factor === 0)) {
+    num = primeFactors.reduce((val, factor) => {
+      if (num % factor === 0) return val / factor;
+      return val;
+    }, num);
+  }
+
+  return num === 1;
+};
+
+console.log(uglyNumAnother(14)); // Output: false
+console.log(uglyNumAnother(6)); // Output: true
+console.log(uglyNumAnother(1)); // Output: true
+console.log(uglyNumAnother(0)); // Output: false
+
+//
+function uglyNumComp(num) {
+  if (num <= 0) return false;
+
+  const primeFactors = [2, 3, 5];
+
+  for (let i = 0; i < primeFactors.length; i++) {
+    while (num % primeFactors[i] === 0) {
+      num /= primeFactors[i];
+    }
+  }
+
+  return num === 1;
+}
+
+console.log(uglyNumComp(14)); // Output: false
+console.log(uglyNumComp(6)); // Output: true
+console.log(uglyNumComp(1)); // Output: true
+console.log(uglyNumComp(0)); // Output: false

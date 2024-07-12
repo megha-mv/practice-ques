f8bf406530ce4e66fb7a7c7b139b6d672f1db235
Megha <megha@nagarro.com>
2024-07-12 11:24:36 +0530

    viewPort and sass learning

---
+++ sasstutorial/gulpfile.js
@@ -0,0 +1,14 @@
+import {src, dest, series, watch } from 'gulp';
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles(){
+    return src('index.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTask(){
+    watch(['index.scss'],buildStyles)
+}
+
+exports.default = series(buildStyles,watchTask);
